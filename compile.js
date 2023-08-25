686c3663b5ea5f40f18d9348da54176e47cee653
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-08-25 16:34:59 +0200

    Refactor `JOBS` setting

--- compile.js
+++ compile.js
@@ -12,17 +12,15 @@ function main() {
     const solcs = fs.readdirSync('.solc')
         .filter(file => file.endsWith('.js'))
         .map(file => file.slice(0, -3))
-        .map(version => ({version, count: fs.readFileSync(`.solc/${version}.hashes.json`, 'utf-8').split('\n').length - 2}));
+        .map(version => ({ version, count: fs.readFileSync(`.solc/${version}.hashes.json`, 'utf-8').split('\n').length - 2 }));
     solcs.forEach(v => console.info(`Queueing compilation for ${c.cyan(v.version)} ${c.magenta(v.count + ' contract hashes')}...`));
-
     solcs.sort((lhs, rhs) => rhs.count - lhs.count);
 
-    const JOBS = 8;
-    const queues = Array.from({ length: JOBS }, () => []);
-    const counts = Array.from({ length: JOBS }, () => 0);
+    const queues = Array.from({ length: 8 }, () => []);
+    const counts = queues.map(() => 0);
     for (let i = 0; i < solcs.length; i++) {
         let q = 0;
-        for (let j = 1; j < JOBS; j++) {
+        for (let j = 1; j < queues.length; j++) {
             if (counts[j] < counts[q]) {
                 q = j;
             }
@@ -31,35 +29,33 @@ function main() {
         counts[q] += solcs[i].count;
     }
 
-    const tasks = Array.from({ length: JOBS }, (_v, q) => {
-        return {
-            title: 'Compiling queue ' + q + '...',
-            task: () => new Observable(async observer => {
-                const queue = queues[q];
-                for (const version of queue) {
-                    await new Promise((resolve, reject) => {
-                        observer.next(version);
-                        const child = fork('./solc.js', [version], { silent: true });
-                        child.on('exit', code => resolve({ version, code }));
-                        child.on('error', reject);
+    const tasks = queues.map((queue, q) => ({
+        title: 'Compiling queue ' + q + '...',
+        task: () => new Observable(async observer => {
+            for (const version of queue) {
+                await new Promise((resolve, reject) => {
+                    observer.next(version);
+                    const child = fork('./solc.js', [version], { silent: true });
+                    child.on('exit', code => resolve({ version, code }));
+                    child.on('error', reject);
 
-                        let lineCount = 0;
-                        let currentLine = '';
-                        child.stdout.on('data', data => {
-                            currentLine += `${data}`;
-                            const lines = `${currentLine}`.split('\n');
-                            currentLine = lines.pop();
+                    let lineCount = 0;
+                    let currentLine = '';
+                    child.stdout.on('data', data => {
+                        currentLine += `${data}`;
+                        const lines = `${currentLine}`.split('\n');
+                        currentLine = lines.pop();
 
-                            lineCount += lines.length;
-                            observer.next(`${formatv(version)}(${lineCount} contracts) ${currentLine}`);
-                        });
+                        lineCount += lines.length;
+                        observer.next(`${formatv(version)}(${lineCount} contracts) ${currentLine}`);
                     });
-                }
+                });
+            }
 
-                observer.complete();
-            })
-        };
-    });
+            observer.complete();
+        })
+    }));
+
     new Listr(tasks, { concurrent: true }).run().catch(err => {
         console.error(err);
     });
